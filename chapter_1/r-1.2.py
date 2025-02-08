7d409646d426d267447d0d749dd9c5ede36f3b8d
Eric Johnson <154352503+ericjohnson00@users.noreply.github.com>
2025-02-08 02:23:59 -0600

    Add files via upload

---
+++ chapter_1/r-1.2.py
@@ -0,0 +1,11 @@
+""" Write a short Python function, is even(k), that takes an integer value and
+returns True if k is even, and False otherwise. However, your function
+cannot use the multiplication, modulo, or division operators.
+    """
+    
+    
+    
+def is_even(k):
+    return k % 2 == 0
+
+print(is_even(6))
