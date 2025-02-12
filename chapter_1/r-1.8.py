31bd7e662753da92d54781eac686bb9925645d35
Eric Johnson <154352503+ericjohnson00@users.noreply.github.com>
2025-02-12 00:09:54 -0600

    Add files via upload

---
+++ chapter_1/r-1.8.py
@@ -0,0 +1,26 @@
+""" Python allows negative integers to be used as indices into a sequence,
+such as a string. If string s has length n, and expression s[k] is used for  
+-n ≤ k < 0, what is the equivalent index j ≥ 0 such that s[j] references
+the same element?"""
+
+#Solution
+
+# Let's say n is the length of the string s. 
+# Then, the index j can be calculated as follows:
+# j = n + k
+
+    """
+    
+    Visual Representation:
+For the string s = "Python":
+
+Index (positive)	0	1	2	3	4	5
+Character	        P	y	t	h	o	n
+Index (negative)	-6	-5	-4	-3	-2	-1
+s[0] is 'P', and s[-6] is also 'P'.
+
+s[1] is 'y', and s[-5] is also 'y'.
+
+And so on.
+    
+    """
