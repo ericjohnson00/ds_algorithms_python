48638e614ad35c44abed52be0119d8ab3bd51cc6
Eric Johnson <ericjohnson8055@gmail.com>
2025-03-08 12:56:14 -0600

    r-2.2

---
+++ chapter_2/r-2.2.py
@@ -0,0 +1,5 @@
+"""Give an example of a software application in which adaptability can mean
+the difference between a prolonged lifetime of sales and bankruptcy."""
+
+# A software application that is used to manage a company's inventory. If the
+# software is not adaptable to changes in the company's inventory management
